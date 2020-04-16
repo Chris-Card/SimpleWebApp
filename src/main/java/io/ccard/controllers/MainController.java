656ad8b08fa2c8c9f6fcd297d8d6caacdb70b6d3
christopher.card <christopher.card@cybergrants.com>
2020-04-15 20:35:08 -0400

    Minor Update to test Jenkins manual build

--- src/main/java/io/ccard/controllers/MainController.java
+++ src/main/java/io/ccard/controllers/MainController.java
@@ -3,6 +3,6 @@ package io.ccard.controllers;
 public class MainController {
 
     public byte[] helloWorld(){
-        return "Hello World! Now I've made a change!".getBytes();
+        return "Hello World! Now I've made a change! And another!".getBytes();
     }
 }

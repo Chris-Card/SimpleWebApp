aa65c48afebe3725873f740ee59471c15b1e82e8
christopher.card <christopher.card@cybergrants.com>
2020-04-15 20:39:00 -0400

    Testing Jenkins polling.

--- src/main/java/io/ccard/controllers/MainController.java
+++ src/main/java/io/ccard/controllers/MainController.java
@@ -3,6 +3,6 @@ package io.ccard.controllers;
 public class MainController {
 
     public byte[] helloWorld(){
-        return "Hello World! Now I've made a change! And another!".getBytes();
+        return "Hello World! Now I've made a change! And another! Now I'm checking for Jenkins polling...".getBytes();
     }
 }
